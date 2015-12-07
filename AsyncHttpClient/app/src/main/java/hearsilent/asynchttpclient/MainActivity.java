f7019f9fdc6360df64986ed172c53ec55770f0e6
HearSilent <hear.silent1995@gmail.com>
2015-12-07 20:48:57 +0800

    Add SwipeRefreshLayout code to AsyncHttpClient.

--- AsyncHttpClient/app/src/main/java/hearsilent/asynchttpclient/MainActivity.java
+++ AsyncHttpClient/app/src/main/java/hearsilent/asynchttpclient/MainActivity.java
@@ -3,6 +3,7 @@ package hearsilent.asynchttpclient;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +11,17 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-		implements CustomListView.OnBottomReachedListener {
+		implements CustomListView.OnBottomReachedListener, SwipeRefreshLayout.OnRefreshListener {
 
 	CustomListView listView;
 	CustomAdapter customAdapter;
+
+	SwipeRefreshLayout swipeRefreshLayout;
 
 	ArrayList<NotifyModule> arrayList = new ArrayList<>();
 	boolean isUpdate = false;
@@ -28,6 +32,11 @@ public class MainActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+		swipeRefreshLayout.setColorSchemeResources(new int[]{R.color.red_500, R.color.blue_500});
+		//		swipeRefreshLayout
+		//				.setColorSchemeColors(new int[]{ContextCompat.getColor(this, R.color.red_500)});
 
 		listView = (CustomListView) findViewById(R.id.listView);
 		listView.setOnBottomReachedListener(this);
@@ -41,15 +50,45 @@ public class MainActivity extends AppCompatActivity
 			}
 		});
 
+		swipeRefreshLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				swipeRefreshLayout.setRefreshing(true);
+			}
+		});
+		swipeRefreshLayout.setOnRefreshListener(this);
+
 		getData();
+	}
+
+	@Override
+	public void onRefresh() {
+		if (!isUpdate) {
+			page = 1;
+			swipeRefreshLayout.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					getData();
+				}
+			}, 2500);
+		}
 	}
 
 	private void getData() {
 		Helper.getNotification(this, page, new GeneralCallback() {
 			@Override
+			public void onFail(String errorMessage) {
+				super.onFail(errorMessage);
+
+				swipeRefreshLayout.setRefreshing(false);
+				Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
 			public void onSuccess(ArrayList<NotifyModule> modules) {
 				super.onSuccess(modules);
-				if (arrayList.size() == 0) {
+
+				if (arrayList.size() == 0 || page == 1) {
 					arrayList = modules;
 					customAdapter = new CustomAdapter();
 					listView.setAdapter(customAdapter);
@@ -59,13 +98,14 @@ public class MainActivity extends AppCompatActivity
 				}
 				page++;
 				isUpdate = false;
+				swipeRefreshLayout.setRefreshing(false);
 			}
 		});
 	}
 
 	@Override
 	public void onBottomReached() {
-		if (!isUpdate) {
+		if (!isUpdate && !swipeRefreshLayout.isRefreshing()) {
 			isUpdate = true;
 			getData();
 		}
