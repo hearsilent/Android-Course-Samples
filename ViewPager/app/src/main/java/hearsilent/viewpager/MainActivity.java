39b161f8832409bd1954ee9b717ab8b101d70a31
HearSilent <hear.silent1995@gmail.com>
2015-12-14 15:58:12 +0800

    Add tabLayout to viewPager.

--- ViewPager/app/src/main/java/hearsilent/viewpager/MainActivity.java
+++ ViewPager/app/src/main/java/hearsilent/viewpager/MainActivity.java
@@ -1,25 +1,58 @@
 package hearsilent.viewpager;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 	ViewPager mViewPager;
+	TabLayout mTabLayout;
+	Toolbar mToolbar;
+
+	MessagesPagerAdapter mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mAdapter = new MessagesPagerAdapter(getSupportFragmentManager());
+
 		mViewPager = (ViewPager) findViewById(R.id.viewPager);
-		mViewPager.setAdapter(new MessagesPagerAdapter(getSupportFragmentManager()));
+		mViewPager.setAdapter(mAdapter);
+
+		// TabLayout
+		mTabLayout = (TabLayout) findViewById(R.id.tabs_main);
+		mTabLayout.setTabsFromPagerAdapter(mAdapter);
+		mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+		mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+			@Override
+			public void onTabSelected(TabLayout.Tab tab) {
+				mViewPager.setCurrentItem(tab.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(TabLayout.Tab tab) {
+
+			}
+
+			@Override
+			public void onTabReselected(TabLayout.Tab tab) {
+
+			}
+		});
+
+		// Toolbar
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(mToolbar);
 
 		// Anim
 		mViewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
@@ -31,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 				textView2.setTranslationX(position * width);
 			}
 		});
+
 	}
 
 	private class MessagesPagerAdapter extends FragmentPagerAdapter {
